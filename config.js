f3d17113952f79e158b3cc5e8235b1c7ff463b3a
Paul Franz <paulkfranz@gmail.com>
2021-08-31 09:58:05 -0400

    header logo adjust

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ var config = {
     markerColor: '#3FB1CE',
     theme: 'light',
     use3dTerrain: true,
-    title: '<img src="images/logo_csd.png" width="200" height="50" /><br /><br />The Chinese Economic Footprint<br />in Central and Eastern Europe',
+    title: '<img src="images/logo_csd_head.png" width="200" height="50" /><br /><br />The Chinese Economic Footprint<br />in Central and Eastern Europe',
     subtitle: '',
     byline: '',
     footer: '<ul class="creditUl"><li><h4><a href="https://csd.bg/publications/publication/chinese-economic-influence-in-europe/" target="_blank">Read the full report</a></h4><a href="https://csd.bg/publications/publication/chinese-economic-influence-in-europe/" target="_blank"><img style="width: 250px;" src="images/report_cover.jpg" /></a></li><li><h4>Credits & Data</h4><strong>Lead Researcher</strong>: Martin Vladimirov<br /><strong>Dataviz & Development</strong>: Paul Franz<br /><strong>Design: </strong>Penu Kiratzov<br /><br />Data:</strong> <a href="https://www.openstreetmap.org" target="_blank">OpenStreetMap</a>, <a href="https://www.mapbox.com" target="_blank">Mapbox</a></li><li><a href="https://www.csd.bg" target="_blank"><img src="images/logo_csd.png" width="300" height="75" /></a><br /><br />&copy; Center for The Study of Democracy, 2021<br /><br /><a href="https://csd.bg/footer-menu/privacy/" target="_blank">Privacy Policy</a></li></ul>',
